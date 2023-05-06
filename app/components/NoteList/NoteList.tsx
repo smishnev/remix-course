ce5016440952679d5e32f91b762fa468ef7d90de
Sergey Mishnev <s.mishnev@forsit.de>
2023-05-06 15:52:08 +0300

    feat: added dynamic router

--- app/components/NoteList/NoteList.tsx
+++ app/components/NoteList/NoteList.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@remix-run/react';
 import styles from './NoteList.css';
 
 type notesPropsItem = {
@@ -11,26 +12,28 @@ function NoteList({ notes }: {notes:notesPropsItem[]} ) {
     <ul id="note-list">
       {notes.map((note, index) => (
         <li key={note.id} className="note">
-          <article>
-            <header>
-              <ul className="note-meta">
-                <li>#{index + 1}</li>
-                <li>
-                  <time dateTime={note.id}>
-                    {new Date(note.id).toLocaleDateString('en-US', {
-                      day: 'numeric',
-                      month: 'short',
-                      year: 'numeric',
-                      hour: '2-digit',
-                      minute: '2-digit',
-                    })}
-                  </time>
-                </li>
-              </ul>
-              <h2>{note.title}</h2>
-            </header>
-            <p>{note.content}</p>
-          </article>
+           <Link to={note.id}>
+            <article>
+              <header>
+                <ul className="note-meta">
+                  <li>#{index + 1}</li>
+                  <li>
+                    <time dateTime={note.id}>
+                      {new Date(note.id).toLocaleDateString('en-US', {
+                        day: 'numeric',
+                        month: 'short',
+                        year: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                      })}
+                    </time>
+                  </li>
+                </ul>
+                <h2>{note.title}</h2>
+              </header>
+              <p>{note.content}</p>
+            </article>
+          </Link>
         </li>
       ))}
     </ul>
---
+++ app/routes/notes.$noteId.tsx
@@ -0,0 +1,42 @@
+import { LoaderArgs, json } from '@remix-run/node';
+import { Link, useLoaderData } from '@remix-run/react';
+import { getStoredNotes } from '~/data/notes';
+import styles from '~/styles/note-details.css';
+
+type notesPropsItem = {
+	id: string;
+	title: string;
+	content: string;
+}
+
+export default function NoteDetailsPage() {
+  const note = useLoaderData();
+
+  return (
+    <main id="note-details">
+      <header>
+        <nav>
+          <Link to="/notes">Back to all Notes</Link>
+        </nav>
+        <h1>{note.title}</h1>
+      </header>
+      <p id="note-details-content">{note.content}</p>
+    </main>
+  );
+}
+
+export async function loader({ params }: LoaderArgs) {
+  const notes: notesPropsItem[] = await getStoredNotes();
+  const noteId = params.noteId;
+  const selectedNote = notes.find(note => note.id === noteId);
+
+if(!selectedNote) {
+  throw json({message: `could not find note for is ${noteId}`}, {status: 404})
+}
+
+  return selectedNote;
+}
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
